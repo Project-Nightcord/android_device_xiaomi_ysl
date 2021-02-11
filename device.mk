1137b3f988e546e367f44f3a44782138f5a18d3e
Alessio Balsini <balsini@google.com>
2021-02-11 10:22:01 +0000

    ysl: k4.19: Use FUSE passthrough by default
    Enable the persist.sys.fuse.passthrough.enable flag in the Redbull
    system configuration to enable the FUSE passthrough feature.
    This feature has been enabled on Cuttlefish devices for about two months
    and no issues have been detected yet.
    
    Bug: 168023149
    Test: 'adb shell getprop | grep persist.sys.fuse.passthrough.enable',
    ScopedStorageTest
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Change-Id: I874e2e2bd143513c32aedf27b824baccc5fcc478

--- device.mk
+++ device.mk
@@ -34,8 +34,14 @@ PRODUCT_PACKAGES += \
     tune2fs_ramdisk \
     resize2fs_ramdisk
 
+ifeq ($(TARGET_KERNEL_VERSION),4.19)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Use FUSE passthrough
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+endif
 
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
